4866572fcdab85896ed8e5048bab0a3461ea4334
yy <yy>
2023-01-24 22:34:25 +0300

    24.01.23

--- login_app/views.py
+++ login_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 
 from django.views import View
 from django.http import HttpResponse
@@ -8,3 +8,6 @@ from django.http import HttpResponse
 class LoginApp(View):
     def get (self, request):
         return render(request, 'login/index.html')
+
+    def post(self, request):
+        return JsonResponse(request.POST, json_dumps_params={'indent': 4})
