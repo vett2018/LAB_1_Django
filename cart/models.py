fe0ba56fbc574df15800dccd42c7ce2e6eeba52a
yy <yy>
2023-01-31 23:46:03 +0300

    31.01.23

--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+from PIL import Image
+
 
 class Cart(models.Model):
    user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -18,6 +19,13 @@ class Product(models.Model):
    image = models.ImageField(upload_to='static/products/', null=True, blank=True)
    created_at = models.DateTimeField(auto_now_add=True)
    updated_at = models.DateTimeField(auto_now=True)
+
+   def save(self, *args, **kwargs):
+      super().save(*args, **kwargs)
+
+      img = Image.open(self.image.path)
+      img = img.resize((100, 100), Image.ANTIALIAS)
+      img.save(self.image.path)
 
    def __str__(self):
        return self.name
