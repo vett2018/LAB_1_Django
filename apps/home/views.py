3ca767ba9afcc9b6b41dda6715b7bfafdd8c9231
yy <yy>
2023-02-08 21:58:56 +0300

    08.02.23

--- apps/home/views.py
+++ apps/home/views.py
@@ -9,6 +9,14 @@ class IndexShopView(View):
    def get(self, request):
        return render(request, 'home/about.html')
 
+   context = {'data': [{'name': 'Bell Pepper',
+                        'discount': 30,
+                        'price_before': 120.00,
+                        'price_after': 80.00,
+                        'url': 'shop/images/product-1.jpg'}
+                       ]
+              }
+
    def get(self, request):
        return render(request, 'home/index.html')
 
